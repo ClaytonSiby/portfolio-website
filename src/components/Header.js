1fdac3950784b3a47afd30b94c732861201d1ad5
ClaytonSiby <claytonsiby@gmail.com>
2021-04-06 13:28:10 +0200

    Import & embed a video tag from the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import styles from '../assets/styles/header.module.css'
+import thevideo from '../assets/videos/hero_bg.mp4'
 
 const Header = () => {
-    return (
-        <div>
-           <h2>Header Here</h2> 
+  return (
+    <Container className={`${styles.header}`}>
+      <header className='v-header'>
+        <div className='fullscreen-video-wrap'>
+          <video autoPlay>
+            {' '}
+            <source src={thevideo} type='video/mp4' />{' '}
+          </video>
         </div>
-    )
+      </header>
+    </Container>
+  )
 }
 
 export default Header
