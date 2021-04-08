7c2706fa6c65d56b22801baea269c1f4a77c440a
ClaytonSiby <claytonsiby@gmail.com>
2021-04-08 13:35:11 +0200

    Add more elements for the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,42 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import styles from '../assets/styles/header.module.css'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import styles from '../assets/styles/header.module.css';
 
 const Header = () => (
   <Container className={`${styles.header} p-0 text-light`}>
     <div className={`${styles.headerDescription}`}>
-      <h3>Hi, I&apos;m Clayton Siby</h3>
-      <h2>Full-Stack Software Developer</h2>
-      <p className='header-text my-3'>
+      <h3 className={`${styles.heroTitle}`}>Hi, I&apos;m Clayton Siby</h3>
+      <h2 className={`${styles.heroStack}`}>Full-Stack Software Developer</h2>
+      <p className={`${styles.headerText} my-3`}>
         I am a Software Enthusiast, I can help you develop web applications.
-        Check out some of my works in the <strong>Projects</strong> section,
+        Check out some of my works in the
+        {' '}
+        <strong>Projects</strong>
+        {' '}
+        section,
         impressed? Then contact me &amp; let&apos;s collaborate!
       </p>
       <p className={`${styles.socialIcons} my-3`}>
         <span className={`${styles.socialIcon} m-1`}>
-          <i className='fab fa-linkedin-in'></i>
-        </span>
-        <span className={`${styles.socialIcon} m-1`}> 
-          <i className='fab fa-github'></i>
+          <a href=""><i className="fab fa-linkedin-in" /></a>
         </span>
         <span className={`${styles.socialIcon} m-1`}>
-          <i className='fab fa-twitter'></i>
+          <a href=""><i className="fab fa-github" /></a>
         </span>
         <span className={`${styles.socialIcon} m-1`}>
-          <i className='fab fa-angellist'></i>
+          <a href=""><i className="fab fa-twitter" /></a>
+        </span>
+        <span className={`${styles.socialIcon} m-1`}>
+          <a href=""><i className="fab fa-angellist" /></a>
         </span>
       </p>
 
       <div className="my-3">
-        <button className="btn btn-md btn-info text-light">Get my Resume</button>
+        <button type="button" className={`${styles.resumeBtn} btn btn-md btn-info text-light`}>Get my Resume</button>
+        <p className={`my-5 text-warning ${styles.workAlert}`}><i>Actively Open for New Opportunities!</i></p>
       </div>
     </div>
   </Container>
-)
+);
 
-export default Header
+export default Header;
