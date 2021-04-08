88774c8d26837a078f9ddb225e9ea62c2949b4d4
ClaytonSiby <claytonsiby@gmail.com>
2021-04-08 20:16:13 +0200

    Add more padding on the services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import styles from '../assets/styles/services.module.css';
 
 const Services = () => (
-  <Container id="services&amp;Technologies" className={`p-0 ${styles.servicesSection}`}>
+  <Container id="services&amp;Technologies" className={`p-0 py-2 ${styles.servicesSection}`}>
     <section>
       <header className="text-center">
         <h3 className="sectionTitle text-light"><i>Services &amp; Technologies</i></h3>
