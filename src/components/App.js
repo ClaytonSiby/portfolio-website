9baa3d810ac63e239f77a9ae2c4380ad6038aacd
ClaytonSiby <claytonsiby@gmail.com>
2021-04-08 13:34:31 +0200

    Update the App component & remove the line break

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './Header';
 import NavBar from './NavBar';
+import Services from './Services';
 
 function App() {
   return (
     <Container>
       <NavBar />
       <Header />
+      <Services />
     </Container>
   );
 }
