0e85ea5a09b0f8704b6383205d3333bc9c0b2649
ClaytonSiby <claytonsiby@gmail.com>
2021-04-08 09:32:59 +0200

    Add initial code for the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,7 @@ import thevideo from '../assets/videos/hero_bg.mp4'
 const Header = () => {
   return (
     <Container className={`${styles.header}`}>
-      <header className='v-header'>
-        <div className='fullscreen-video-wrap'>
-          <video autoPlay>
-            {' '}
-            <source src={thevideo} type='video/mp4' />{' '}
-          </video>
-        </div>
-      </header>
+      
     </Container>
   )
 }
