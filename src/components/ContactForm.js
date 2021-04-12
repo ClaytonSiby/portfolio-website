bec8b592444983a2321796db4f5ebf8dc178e1ce
ClaytonSiby <claytonsiby@gmail.com>
2021-04-12 02:20:40 +0200

    Fix all eslint errors

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,54 +1,61 @@
-import React from 'react'
-import { Container, Col, Row, Form, Button } from 'react-bootstrap'
-import styles from '../assets/styles/contactform.module.css'
+import React from 'react';
+import {
+  Container, Col, Row, Form, Button,
+} from 'react-bootstrap';
+import styles from '../assets/styles/contactform.module.css';
 
 const ContactForm = () => (
   <Container
-    id='services&amp;Technologies'
+    id="services&amp;Technologies"
     className={`p-0 mt-3 py-2 ${styles.contactForm}`}
   >
-    <header className='text-center'>
-      <h3 className='sectionTitle text-light'>
+    <header className="text-center">
+      <h3 className="sectionTitle text-light">
         <i>Contact Me</i>
       </h3>
     </header>
     <p className={`m-2 text-info ${styles.connect}`}>Let&apos;s connect!</p>
-    <p className='text-info'>
-      <b>email:</b> claytonsiby@gmail.com <b>cell:</b> +27 64 122 6401
+    <p className="text-info">
+      <b>email:</b>
+      {' '}
+      claytonsiby@gmail.com
+      <b>cell:</b>
+      {' '}
+      +27 64 122 6401
     </p>
     <Form
-      action='https://formspree.io/f/mwkakzbr'
-      method='POST'
-      className='m-2'
+      action="https://formspree.io/f/mwkakzbr"
+      method="POST"
+      className="m-2"
     >
       <Row>
-        <Col xs={12} sm={12} md={6} className='my-1'>
-          <Form.Control type='text' name='User' placeholder='Username' />
+        <Col xs={12} sm={12} md={6} className="my-1">
+          <Form.Control type="text" name="User" placeholder="Username" />
         </Col>
-        <Col xs={12} sm={12} md={6} className='my-1'>
+        <Col xs={12} sm={12} md={6} className="my-1">
           <Form.Control
-            type='email'
-            name='_replyto'
-            placeholder='Email Address'
+            type="email"
+            name="_replyto"
+            placeholder="Email Address"
           />
         </Col>
 
-        <Col className='my-2'>
+        <Col className="my-2">
           <Form.Group>
             <Form.Control
-              name='message'
-              as='textarea'
+              name="message"
+              as="textarea"
               rows={4}
-              placeholder='Leave a Message'
+              placeholder="Leave a Message"
             />
           </Form.Group>
-          <Button type="submit" className='btn-block' variant='info' type='submit'>
+          <Button className="btn-block" variant="info" type="submit">
             Send Message
           </Button>
         </Col>
       </Row>
     </Form>
   </Container>
-)
+);
 
-export default ContactForm
+export default ContactForm;
